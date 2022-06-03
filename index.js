230c295625e09900a53aa3eb28e4140b5c806e1e
Juanqui <jcf.daw@gmail.com>
2022-06-03 20:10:10 +0200

    Tema 5 Node OB

---
+++ index.js
@@ -0,0 +1,9 @@
+let alturaCM = 180;
+let alturaMetros = 1.80;
+let pesoK = 80.0;
+let alturaR = Math.ceil(alturaMetros);
+let pesoR = Math.floor(pesoK);
+
+if(Math.max() === (Math.max() + 1)) {
+    console.log("Verdadero");
+}
